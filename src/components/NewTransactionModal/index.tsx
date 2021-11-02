a70a4c55df02a13e128a1e8cf6f0a4beb96bb98e
Alysson Bormann <alysson.bormann@gmail.com>
2021-11-02 15:29:05 -0400

    funcionalidades dos botoes e cores botoes

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 interface NewTransactionProps {
   isOpen: boolean;
@@ -13,6 +14,8 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionProps) {
+  const [type, setType] = useState("deposit");
+
   return (
     <Modal
       isOpen={isOpen}
@@ -35,21 +38,35 @@ export function NewTransactionModal({
 
         <input placeholder="Valor" type="number" />
 
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("deposit");
+            }}
+            isActive={type === "deposit"}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("withdraw");
+            }}
+            isActive={type === "withdraw"}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
-
-        <TransactionTypeContainer>
-          <button type="button">
-            <img src={incomeImg} alt="Entrada" />
-            <span>Entrada</span>
-          </button>
-
-          <button type="button">
-            <img src={outcomeImg} alt="Saída" />
-            <span>Saída</span>
-          </button>
-        </TransactionTypeContainer>
       </Container>
     </Modal>
   );
